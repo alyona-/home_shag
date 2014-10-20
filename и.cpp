fb6c022f3e1e436078d25a42817a1461721592b1
alyona- <alyona_om@mail.ru>
2014-10-20 13:33:08 +0400

    add all

---
+++ и.cpp
@@ -0,0 +1,21 @@
+/*и*/
+#include  <iostream>
+
+
+using namespace std;
+
+void main() {
+	setlocale(LC_ALL, "russian");
+	int x;
+	cout << "Введите сторону квадрата: \n";
+	cin >> x;
+	for (int i = 1; i <= x; i++){
+
+		for (int j = 1; j <= x; j++){
+			if (j <= x-i) cout << "*";
+			else cout << " ";
+		}
+		cout << "\n";
+	}
+
+}
